9d2c533ad595862cf371bd9a6bdf82eb08dbe2a5
tomhyhan <tomhyhan@gmail.com>
2024-01-25 16:02:33 -0800

    backprop

--- ANN/digit/network1.py
+++ ANN/digit/network1.py
@@ -26,10 +26,30 @@ class Network:
         3. backpropagate the error: compute the previous layer's delta using the last layer delta
         4. output: compute the gradient of the cost function 
         """
+        # 1
         nabla_b = [np.zeros(b.shape) for b in self.biases]
         nabla_w = [np.zeros(w.shape) for w in self.weights]
+        activation = root_activations
         activations = [root_activations]
         zs = []
+        for w, b in zip(self.weights, self.biases):
+            z = np.dot(w, activation) + b
+            activation = sigmoid(z)
+            zs.append(z)
+            activations.append(activation)
+        # 2
+        delta = (activation - validations) * sigmoid_prime(zs[-1])
+        nabla_b[-1] = delta
+        # print(delta.shape, activations[-2].shape)
+        # for a in activations:
+        #     print(a.shape)
+        # print("normal", activations[-2])
+        print("delta", delta)
+        print("trans", activations[-2].transpose())
+        # print(np.dot(delta, activations[-2].transpose()))
+        nabla_w[-1] = np.dot(delta, activations[-2].transpose())
+
+        # print("z", z)
         return nabla_w, nabla_b
 
     def update_mini_batch(self, mini_batch, learning_rate):
@@ -40,7 +60,6 @@ class Network:
         3. Use the result from backpropagation to perform gradient descent algorithm to adjust the weight and biases from each layer.
         """
         mini_batch_len = len(mini_batch)
-        print(self.biases)
         nabla_b = [np.zeros(b.shape) for b in self.biases]
         nabla_w = [np.zeros(w.shape) for w in self.weights]
         for root_activations, validations in mini_batch:
@@ -48,6 +67,7 @@ class Network:
                 root_activations, validations)
             nabla_b = [b + dnb for b, dnb in zip(nabla_b, delta_nabla_b)]
             nabla_w = [w + dnw for w, dnw in zip(nabla_w, delta_nabla_w)]
+            break
         # apply gradient descent nabla C = (w, b)T
         # change in v = - learning rate * nabla
         # print("weight", self.weights)
@@ -84,8 +104,7 @@ class Network:
                 self.update_mini_batch(mini_batch, learning_rate)
                 break
             if test_data:
-                print(f"Epoch {epoch} :{self.evaluate(
-                    test_data)} / {test_data_length}")
+                print(f"Epoch {epoch} :{self.evaluate(test_data)} / {test_data_length}")
                 pass
             else:
                 print(f"Epoch: {epoch} complete!")
@@ -104,12 +123,20 @@ class Network:
                           for image, num in test_data]
         return sum(predict == output for (predict, output) in testing_result)
 
-
 def sigmoid(z):
     return 1.0 / (1.0 + np.exp(-z))
+
+def sigmoid_prime(z):
+    return sigmoid(z) * (1 - sigmoid(z))
 
 
 training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
 network = Network([28 * 28, 5, 10])
 # training_data, epochs, mini_batch_size, learning_rate, test_data=None
-# print(network.SGD(training_data, 1, 100, 1, test_data))
+print(network.SGD(training_data, 1, 10, 1, test_data))
+
+# x = np.array([np.array([1,2])])
+# y = np.array([np.array([[3],[4]])])
+# # print(np.dot(x,y))
+# print(np.dot(y,x))
+# print(np.dot(x,y))
