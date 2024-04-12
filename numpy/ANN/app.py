3f52d7adefb0052c589248c63e33323d82b8ef9f
tomhyhan <tomhyhan@gmail.com>
2024-04-12 15:52:55 -0700

    adding softmax

--- numpy/ANN/app.py
+++ numpy/ANN/app.py
@@ -1,7 +1,7 @@
 import numpy as np
-from tensorflow.keras.datasets import fashion_mnist
+from tensorflow.keras.datasets import mnist
 import random
-from utils import ReLU, Sigmoid, cost_function, loss_function, ReLU_prime, Sigmoid_prime
+from utils import ReLU, Sigmoid, cost_function, loss_function, ReLU_prime, Sigmoid_prime, simple_loss_function, convert_one_hot
 
 N_TRAIN_SAMPLES = 5000
 # N_TRAIN_SAMPLES = 50000
@@ -10,13 +10,13 @@ N_VALID_SAMPLES = 250
 N_CLASSES = 10
 IMAGE_SIZE = 28
 
-((train_x, train_y), (test_x, test_y))  = fashion_mnist.load_data()
+((train_x, train_y), (test_x, test_y))  = mnist.load_data()
 
-train_data = [(tx, ty) for tx, ty in zip(train_x[:N_TRAIN_SAMPLES, :, :], train_y[:N_TRAIN_SAMPLES])]
+train_data = [(tx / 255, ty) for tx, ty in zip(train_x[:N_TRAIN_SAMPLES, :, :], train_y[:N_TRAIN_SAMPLES])]
 
 test_data = [(tx, ty) for tx, ty in zip(train_x[N_TRAIN_SAMPLES:N_TRAIN_SAMPLES+N_TEST_SAMPLE, :, :], train_y[:N_TEST_SAMPLE])]
 
-valid_data = [(vx, vy) for vx, vy in zip(test_x[:N_VALID_SAMPLES, :, :],test_y[:N_VALID_SAMPLES])]
+valid_data = [(vx / 255, vy) for vx, vy in zip(test_x[:N_VALID_SAMPLES, :, :],test_y[:N_VALID_SAMPLES])]
 
 NN_ARCHITECTURE = [
     {"input_dim": 784, "output_dim": 30, "activation": "relu"},
@@ -29,6 +29,7 @@ def init_layers(nn_architecture, seed = None):
 
     weights = []
     biases = []
+    activation_fns = []
     for arch in nn_architecture:
         w = np.random.randn(arch["output_dim"], arch["input_dim"]) / np.sqrt(arch["input_dim"])
         b = np.random.randn(arch["output_dim"], 1)
@@ -36,7 +37,7 @@ def init_layers(nn_architecture, seed = None):
         biases.append(b)
     return weights, biases
 
-def full_forward_propagation(images, weights, biases, activate):
+def full_forward_propagation(images, weights, biases, activation_fn):
     zs = []
     activations = []
     activations.append(images)
@@ -45,7 +46,7 @@ def full_forward_propagation(images, weights, biases, activate):
     for w,b in zip(weights, biases):
         z = np.matmul(w, a) + b
         zs.append(z)
-        a = activate(z)
+        a = activation_fn(z)
         activations.append(a)
     return a, zs, activations
 
@@ -53,63 +54,59 @@ def backprop(predict, targets, zs, activations, weights, biases, nn_architecture
     nabla_w = [np.zeros(w.shape) for w in weights]
     nabla_b = [np.zeros(b.shape) for b in biases]
 
-    targets = targets.reshape(-1, 1)
-    delta = loss_function(predict, targets) * ReLU_prime(zs[-1])
-    # delta = loss_function(predict, targets) * Sigmoid_prime(zs[-1])
-    
-    nabla_w[-1] = np.sum(np.matmul(delta, np.transpose(activations[-2], (0,2,1))) , axis=0) 
-    nabla_b[-1] = np.sum(delta , axis=0)
-    # nabla_b -1 (10, 1)
-    # nabla_w -1 (10, 30)
-    # delta: (10, 10, 1)
-    # nabla_b -1 (30, 1)
-    # nabla_w -1 (30, 784)
-    # delta: (10, 30, 1)
-    for l in range(2, len(nn_architecture) + 1):
-        predict = activations[-l]
-        curr_weights = weights[-l+1]
-        
-        delta = np.matmul(curr_weights.transpose(), delta)
+    targets = np.array(list(map(convert_one_hot, targets)))
+    # print(targets.shape)
 
-        nabla_w[-l] = np.sum(np.matmul(delta, np.transpose(activations[-l-1], (0,2,1))), axis=0) 
-        nabla_b[-l] = np.sum(delta, axis=0) 
+    delta = simple_loss_function(predict, targets)
+    # delta = loss_function(predict, targets) * Sigmoid_prime(zs[-1])
+    # delta = loss_function(predict, targets) * ReLU_prime(zs[-1])
+    
+    nabla_b[-1] = np.sum(delta , axis=0)
+    nabla_w[-1] = np.sum(np.matmul(delta, np.transpose(activations[-2], (0,2,1))) , axis=0) 
+
+
+    for l in range(2, len(nn_architecture) + 1):
+        z = zs[-l]
+        # sp = Sigmoid_prime(z)
+
+        delta = np.matmul(weights[-l+1].transpose(), delta) 
+        # delta = np.matmul(weights[-l+1].transpose(), delta) * sp
+
+        nabla_b[-l] = np.sum(delta, axis=0)
+        nabla_w[-l] = np.sum(np.matmul(delta, np.transpose(activations[-l-1], (0,2,1))), axis=0)
 
     return nabla_b, nabla_w
         
 def step(weights, biases, nabla_b, nabla_w, lr, batch_size):
-    weights = [(1-lr*(0.05 / batch_size)) * w - (lr / batch_size) * nw for w, nw in zip(weights, nabla_w)]
-    biases = [b - (lr / batch_size) * nb for b, nb in zip(biases, nabla_b)]
-    return weights, biases
+    nweights = [(1-lr*(0.05 / batch_size)) * w - (lr / batch_size) * nw for w, nw in zip(weights, nabla_w)]
+    nbiases = [b - (lr / batch_size) * nb for b, nb in zip(biases, nabla_b)]
+    return nweights, nbiases
 
-def clac_accuracy(predict, targets_test):
-    # print("argmax:", np.argmax(a, axis=1).shape)
-    # print(y.shape)
-    # print(np.argmax(a, axis=1) == y)
-    # print(np.argmax(a[0], axis=1) == y[0])
-    # return np.sum(np.argmax(a, axis=1) == y)
-    print(predict[0])
-    print("argmax:", np.argmax(predict[0]))
-    results = [(np.argmax(x), y)
-                        for (x, y) in zip(predict, targets_test)]
-    # print(results[0])
-    return sum([int(a == y) for a, y in results])
+def feedforward(activation, weights, biases):
+    for w, b in zip(weights, biases):
+        activation = Sigmoid(np.dot(w, activation) + b)
+    return activation
 
-def sgd(train_data, test_data, valid_data, nn_architecture, epochs, lr, batch_size=10, activation_fn=ReLU):
+def clac_accuracy(data, weights, biases):
+    results = [(np.argmax(feedforward(x.reshape(784, 1), weights, biases)), y)
+                        for (x, y) in data]
+    return sum(int(x == y) for (x, y) in results)
+
+def sgd(train_data, test_data, valid_data, nn_architecture, epochs, lr, activation_fn, batch_size=10):
     weights, biases = init_layers(nn_architecture)
-    len_t = len(train_data)
-    # print(weights[0])
 
     for epoch in range(epochs):
         random.shuffle(train_data)
         mini_batches = [train_data[k: k + batch_size] for k in range(0,N_TRAIN_SAMPLES,batch_size)]
         print(f"Epoch {epoch} has started")
-        # print(len(mini_batches))
+
         for i, mini_batch in enumerate(mini_batches):
             if i % 1000 == 0:
                 print(f"{i * batch_size} has been trained" )
+
             images = np.array([image for image, _ in mini_batch]).reshape(batch_size, 784, 1)
             targets = np.array([target for _, target in mini_batch])
-            # print(images.shape, targets.shape)
+
             # feed forward
             predict, zs, activations = full_forward_propagation(images, weights, biases, activation_fn)
 
@@ -119,16 +116,14 @@ def sgd(train_data, test_data, valid_data, nn_architecture, epochs, lr, batch_si
             # update
             weights, biases = step(weights, biases, nabla_b, nabla_w, lr, batch_size)
 
-            # print(weights[0])
-        # print(weights[0])
-        # print(np.array([image for image in valid_data[0]]).shape)
-    valid_images = np.array([image for image, _ in valid_data]).reshape(N_VALID_SAMPLES, 784, 1)
-    valid_targets = np.array([y for _, y in valid_data])
-    predict, _, _ = full_forward_propagation(valid_images, weights, biases, activation_fn)
-    # print(targets_test.shape, predict.shape)
-    result = clac_accuracy(predict, valid_targets)
-    print(f"corrections: {result} / {N_VALID_SAMPLES}")
-    print(f"accuracy: {result / N_VALID_SAMPLES}")
-    # cost = cost_function(predict, valid_targets) / N_VALID_SAMPLES
+        result = clac_accuracy(valid_data, weights, biases)
+        print(f"corrections: {result} / {N_VALID_SAMPLES}")
+        print(f"accuracy: {result / N_VALID_SAMPLES}")
+        # cost = cost_function(predict, valid_targets) / N_VALID_SAMPLES
     # print(f"cost: {cost}")
-sgd(train_data, test_data, valid_data, NN_ARCHITECTURE, 3, 0.3, batch_size=10, activation_fn=ReLU)
+
+# apply softmax and ensure dims stay same
+    
+sgd(train_data , test_data, valid_data, NN_ARCHITECTURE, 10, 0.005, Sigmoid, batch_size=10)
+
+# print(train_data[0][0])
--- numpy/ANN/utils.py
+++ numpy/ANN/utils.py
@@ -2,8 +2,11 @@ import numpy as np
 
 eps=1e-8
 
-def Sigmoid(Z):
-    return 1 / 1 + np.exp(-Z)
+def Sigmoid(z):
+    # print(z)
+    # if (1 + np.exp(-z)).any():
+    #     print(z)
+    return 1 / (1 + np.exp(-z))
 
 def Sigmoid_prime(z):
     return Sigmoid(z) * (1 - Sigmoid(z))
@@ -17,5 +20,23 @@ def ReLU_prime(Z):
 def cost_function(a, y):
     return np.sum(np.nan_to_num(-y*np.log(a)-(1-y)*np.log(1-a))) 
 
+def softmax(z):
+  e = np.exp(z - np.max(z))
+  return e / np.sum(e, axis=0)
+
+# Example usage
+z = np.array([1, 2, 3, 4, 5])
+scores = softmax(z)
+print(scores)
+
+
 def loss_function(a, y):
     return - (np.divide(y,np.maximum(a,eps)) - np.divide(1-y, 1-np.maximum(a,eps)))
+
+def simple_loss_function(a, y):
+    return a - y
+
+def convert_one_hot(n):
+    hot = np.zeros((10,1))
+    hot[n] = 1.0
+    return hot
